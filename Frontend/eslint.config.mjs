85b55d4f8f859f883543571fc9b23dd49cf421aa
nlokeshbabu1 <lokeshbabu.nalluri1111@gmail.com>
2025-06-30 14:38:17 +0530

    fix: eslint

--- Frontend/eslint.config.mjs
+++ Frontend/eslint.config.mjs
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-import js from "@eslint/js";
-import globals from "globals";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
-
-
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
-  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
-]);
-=======
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import pluginReact from 'eslint-plugin-react'; // Import the plugin directly
@@ -103,4 +89,3 @@ export default [
     }
   },
 ];
->>>>>>> feature
