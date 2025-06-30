9f0c86d6244ae70e191d1a14ed51cb5915309e62
nlokeshbabu1 <lokeshbabu.nalluri1111@gmail.com>
2025-06-30 14:29:02 +0530

    Fix: frontend bugs

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,23 +1,12 @@
-<<<<<<< HEAD
-/** @type {import('tailwindcss').Config} */ // This comment is fine
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-=======
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     './index.html',
     './src/**/*.{js,ts,jsx,tsx}',
->>>>>>> feature
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-<<<<<<< HEAD
-}
-=======
 };
   
->>>>>>> feature
