6680be8aec870980f3f630a46289e3bfaea15f10
nlokeshbabu1 <lokeshbabu.nalluri1111@gmail.com>
2025-07-31 16:36:43 +0530

    updated babel file

--- Frontend/babel.config.js
+++ Frontend/babel.config.js
@@ -1,10 +1,9 @@
-// babel.config.js
 module.exports = {
-    presets: [
-      // Transpile modern JS features for the current Node.js version Jest runs on
-      ['@babel/preset-env', { targets: { node: 'current' } }],
-      // Transpile React JSX syntax. 'runtime: "automatic"' is for React 17+
-      // If using an older React version, you might need 'runtime: "classic"'
-      ['@babel/preset-react', { runtime: 'automatic' }],
-    ],
-  };
+  presets: [
+    // Transpile modern JS features for the current Node.js version Jest runs on
+    ['@babel/preset-env', { targets: { node: 'current' } }],
+    // Transpile React JSX syntax. 'runtime: "automatic"' is for React 17+
+    // If using an older React version, you might need 'runtime: "classic"'
+    ['@babel/preset-react', { runtime: 'automatic' }],
+  ],
+};
