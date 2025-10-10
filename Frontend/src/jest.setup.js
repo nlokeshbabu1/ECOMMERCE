53d1a820e6fd9953e31bac396a3a0c7b4d592197
nlokeshbabu1 <lokeshbabu.nalluri1111@gmail.com>
2025-10-10 07:54:53 +0000

    updated the lint error in jest file

--- Frontend/src/jest.setup.js
+++ Frontend/src/jest.setup.js
@@ -1,9 +1,9 @@
 // Mock import.meta for Jest
 Object.defineProperty(global, 'import.meta', {
-    value: {
-      env: {
-        VITE_API_URL: 'http://localhost:5000',
-      },
+  value: {
+    env: {
+      VITE_API_URL: 'http://localhost:5000',
     },
-    writable: true,
-  });
+  },
+  writable: true,
+});
--- Frontend/src/setupTests.js
+++ Frontend/src/setupTests.js
@@ -5,13 +5,13 @@ global.TextDecoder = TextDecoder;
 
 // Mock import.meta for Jest
 Object.defineProperty(global, 'import.meta', {
-    value: {
-      env: {
-        VITE_API_URL: 'http://localhost:5000',
-      },
+  value: {
+    env: {
+      VITE_API_URL: 'http://localhost:5000',
     },
-    writable: true,
-  });
+  },
+  writable: true,
+});
 
 // src/setupTests.js
 import '@testing-library/jest-dom';
